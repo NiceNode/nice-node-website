27edbd290b00710dd2b4f6198b4d68325dcbcba2
corn-potage <cornpotage@protonmail.com>
2022-12-14 00:01:37 -0800

    added header, icons, replaced toggle

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,13 @@ import '../sass/index.scss';
 
 document.addEventListener("DOMContentLoaded", function () {
   // modify this later with localCache, local settings detection
-  const checkbox = document.querySelector('input[name=mode]');
+  const checkbox = document.querySelector('li.switch');
   const body = document.querySelector('body');
+  let currentTheme = body.classList.contains("light") ? "light" : "dark";
 
-  checkbox.addEventListener("change", function () {
-    if(this.checked) {
-      trans();
-      body.classList.replace('light', 'dark');
-    } else {
-        trans();
-        body.classList.replace('dark', 'light');
-    }
+  checkbox.addEventListener("click", function () {
+    const nextTheme = currentTheme === "light" ? "dark" : "light";
+    body.classList.replace(currentTheme, nextTheme);
+    currentTheme = nextTheme;
   });
 });
-
-
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition');
-    }, 1000);
-};
