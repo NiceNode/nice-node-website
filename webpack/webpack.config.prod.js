9d9b10ffbecc99d5ead7359460bddbd9ab4e9415
jgresham <johnsgresham@gmail.com>
2023-10-20 11:12:13 -0700

    test dont stringify procces.env

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,9 +4,6 @@ const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
-console.log("process.env: ", process.env);
-console.log("process.env.MIXPANEL_TOKEN: ", process.env.MIXPANEL_TOKEN);
-
 module.exports = merge(common, {
   entry: {
     app: Path.resolve(__dirname, '../src/js/index.js'),
@@ -24,7 +21,7 @@ module.exports = merge(common, {
       // Strings need to be stringified here
       // See https://webpack.js.org/plugins/define-plugin/#usage for more
       'process.env.NODE_ENV': JSON.stringify('production'),
-      'process.env.MIXPANEL_TOKEN' : JSON.stringify(process.env.MIXPANEL_TOKEN)
+      'process.env.MIXPANEL_TOKEN' : process.env.MIXPANEL_TOKEN
     }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[chunkhash:8].css',
